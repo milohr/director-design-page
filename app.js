89db11d6b901941694e0c64eeb11cd709b3c9d3f
camilo <milo.h@aol.com>
2017-01-24 11:17:46 -0500

    first attempts

---
+++ app.js
@@ -0,0 +1,56 @@
+function waitThreeSeconds()
+{
+    var ms= 3000 +new Date().getTime();
+    while (new Date()< ms){}
+    console.log("finished fuction");
+}
+
+function calculation()
+{
+    var a = 3+2*5 + "hello bitches";
+    console.log(a);
+}
+
+
+function greet(name)
+{
+    name=name || "unkown" || name.item;
+    console.log("Hello "+name);
+}
+
+
+
+function clicked()
+{
+    //document.getElementById("btn").innerHTML+= ":)";
+    console.log("logo was clicked");
+    
+}
+
+function hovered()
+{
+    document.getElementById("fondo").className = 'newclass';
+    console.log("logo was hovered");
+    
+}
+
+function unhovered()
+{
+    document.getElementById("fondo").className = 'oldclass';
+    console.log("logo was unhovered");
+    
+}
+
+//listen for the click event
+document.getElementById("logo").addEventListener("click", clicked);
+document.getElementById("logo").addEventListener("mouseover",hovered);
+document.getElementById("logo").addEventListener("mouseout",unhovered);
+
+//waitThreeSeconds();
+/*calculation();
+greet();
+greet("camilo");
+
+console.log(libraryName);
+
+console.log("finished exec");*/
