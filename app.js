14ffda8e71a0520af6787f5750b799516302b8ab
camilo <milo.h@aol.com>
2017-01-25 14:43:29 -0500

    projects page

--- app.js
+++ app.js
@@ -24,7 +24,21 @@ function clicked()
 {
     //document.getElementById("btn").innerHTML+= ":)";
     console.log("logo was clicked");
-    document.getElementById("logo").className = 'rotate';
+   /* document.getElementById("logo").className = 'rotate';*/
+   
+        if(i===images.length) i=0;
+        console.log('url("'+images[i]+'") no-repeat center center fixed;');
+        var image = "url(\""+images[i]+"\") no-repeat center center fixed";
+        document.getElementById("fondo").style.background= image;
+        document.getElementById("fondo").style.backgroundSize="cover";
+       
+       
+        i++;
+        
+        
+
+        
+        
     
 }
 
@@ -32,7 +46,7 @@ function hovered()
 {
     document.getElementById("fondo").className = 'newclass';
     document.getElementById("menu").className = 'show';
-    
+    document.getElementById("logo").style.cursor="pointer";
     console.log("logo was hovered");
     
 }
@@ -44,6 +58,10 @@ function unhovered()
     console.log("logo was unhovered");
     
 }
+var i=0;
+
+var images = ["imagenesprueba/elcrimendejuaquin2.png","imagenesprueba/elcrimendejuaquin.png","imagenesprueba/laruleta2.png","imagenesprueba/videomusical.png","imagenesprueba/crimen_joaquin.png"]
+
 
 //listen for the click event
 document.getElementById("logo").addEventListener("click", clicked);
